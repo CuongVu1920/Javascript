eb1cd1fa07c53e9dfe7143fdee565ea319056437
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-11-12 16:00:41 +0700

    Primitive và Reference types

---
+++ ObjectAdvan/Primitive và Reference types/main.js
@@ -0,0 +1,18 @@
+// let person = {
+//   name: 'John'
+// };
+
+// let personCopy = person;
+
+// personCopy.name = "Bob";
+
+// console.log(person.name); // "Bob"; 
+
+let person = {
+  name: 'John'
+};
+
+let personCopy = person;
+
+console.log(person === personCopy); // true
+
