d048e9473bd8a569d4b7139722212723516fd2e8
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-11-21 19:57:02 +0700

    Function constructor

--- ObjectAdvan/Function constructor/main.js
+++ ObjectAdvan/Function constructor/main.js
@@ -6,38 +6,30 @@
  * defense: phòng thủ
  */
 
-// Object literal
+// Function/ Object constructor (Hàm tạo - bản thiết kế đối tượng)
 
-const tom = {
-  name: "Tom",
-  hp: 1000,
-  atk: 50,
-  defense: 5,
-  attack(target) {
+// Bản thiết kế nhân vật
+function Character(name, hp, atk, defense) {
+  this.name = name;
+  this.hp = hp;
+  this.atk = atk;
+  this.defense = defense;
+  this.attack = function (target) {
     const damage = Math.max(this.atk - target.defense, 0);
     target.hp -= damage;
     console.log(`${this.name} "phang" ${target.name} với ${damage} sát thương, khiến cho ${target.name} còn ${target.hp} máu`);
-  },
-  isAlive() {
+  };
+
+  this.isAlive = function () {
     return this.hp > 0;
   }
 }
 
-const jerry = {
-  name: "Jerry",
-  hp: 500,
-  atk: 20,
-  defense: 20,
-  attack(target) {
-    const damage = Math.max(this.atk - target.defense, 0);
-    target.hp -= damage;
-    console.log(`${this.name} "phang" ${target.name} với ${damage} sát thương, khiến cho ${target.name} còn ${target.hp} máu`);
-  },
-  isAlive() {
-    return this.hp > 0;
-  }
-}
+// Tạo ra nhân vật Tom
+const tom = new Character("Tom", 1000, 50, 5);
 
+// Tạo ra nhân vật Jerry
+const jerry = new Character("Jerry", 500, 20, 20);
 
 let round = 1;
 while(tom.isAlive() && jerry.isAlive()) {
