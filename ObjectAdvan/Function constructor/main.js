6adf4d6db15d45eac3e63b7421f49b73bbe1f79e
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-11-24 19:40:01 +0700

    Thêm chỉ số phản công

--- ObjectAdvan/Function constructor/main.js
+++ ObjectAdvan/Function constructor/main.js
@@ -1,4 +1,4 @@
-// Tom và Jerry
+// Tom và Jerry(counterRate: 0.8)
 
 /**
  * Health / HP: Máu
@@ -8,16 +8,28 @@
 
 // Function/ Object constructor (Hàm tạo - bản thiết kế đối tượng)
 
+// Bài tiếp theo - Phát triển chỉ số "phản công"
+
 // Bản thiết kế nhân vật
-function Character(name, hp, atk, defense) {
+function Character(name, hp, atk, defense,speed, counterRate) {
   this.name = name;
   this.hp = hp;
   this.atk = atk;
+  this.speed = speed;
   this.defense = defense;
+  
+  this.counterRate = counterRate;
   this.attack = function (target) {
     const damage = Math.max(this.atk - target.defense, 0);
     target.hp -= damage;
     console.log(`${this.name} "phang" ${target.name} với ${damage} sát thương, khiến cho ${target.name} còn ${target.hp} máu`);
+
+    // Xử lý target phản công
+    if(target.isAlive() && Math.random() < target.counterRate) {
+      const counterDamage = Math.max(target.atk - this.defense, 0);
+      this.hp -= counterDamage;
+      console.log(`${target.name} "phản công" ${this.name} với ${counterDamage} sát thương, khiến cho ${this.name} còn ${this.hp} máu`);
+    }
   };
 
   this.isAlive = function () {
@@ -26,26 +38,35 @@ function Character(name, hp, atk, defense) {
 }
 
 // Tạo ra nhân vật Tom
-const tom = new Character("Tom", 1000, 50, 5);
+const tom = new Character("Tom", 1000, 50, 5, 30, 0.5);
 
 // Tạo ra nhân vật Jerry
-const jerry = new Character("Jerry", 500, 20, 20);
+const jerry = new Character("Jerry", 500, 20, 20,10, 0.2);
 
-let round = 1;
-while(tom.isAlive() && jerry.isAlive()) {
-  console.log(`Round - ${round}:`);
-  
-  if(round % 2 === 0) {
-    tom.attack(jerry);
-  }else {
-    jerry.attack(tom);
+function battle(char1, char2) {
+  let round = 1;
+
+  while (char1.isAlive() && char2.isAlive()) {
+    console.log(`Round: ${round}`);
+    
+    if(char1.speed >= char2.speed)
+      char1.attack(char2);
+      if(char2.isAlive()) {
+        char2.attack(char1);
+      }else {
+        char2.attack(char1);
+        if(char1.isAlive()) {
+          char1.attack(char2);
+        }
+      }
+
+      round++;
   }
 
-  round++;
+  const winer = char1.isAlive() ? char1 : char2;
+  console.log(`${winer.name} là kẻ chiến thắng`);
+  return winer;
 }
 
-if(tom.isAlive()) {
-  console.log(`${tom.name} là kẻ chiến thắng`);
-}else {
-  console.log(`${jerry.name} là kẻ chiến thắng`);
-}
+battle(tom, jerry)
+
