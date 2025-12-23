8be517ef4062d88d98695bf72f5be889da8d4f2a
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-12-23 16:25:42 +0700

    Tổng quan làm việc với chuỗi

---
+++ String/Tổng quan làm việc với chuỗi/main.js
@@ -0,0 +1,11 @@
+// Tổng quan làm việc với chuỗi => console.log(String.prototype);
+
+// truy cập chuỗi
+
+let email = "nguyenvana@gmail.com";
+
+// - string.length, string['length']
+// - string[index], string.charAt(index), string.at(index)
+
+console.log(email.charAt(9));
+
