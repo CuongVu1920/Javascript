da9069d36aa7af21c4098c34d649b088eb0f8029
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-02 16:03:41 +0700

    Thay thế nội dung chuỗi

---
+++ String/Thay thế nội dung chuỗi/main.js
@@ -0,0 +1,10 @@
+// Thay thế nội dung chuỗi
+
+// replace()
+// replaceAll
+
+let sentence = "Tôi yêu mÈo mèo";
+
+// Kiểu nguyên thủy là bất biến - nghĩa là khi tạo ra giá trị nguyên thủy thì nó sẽ k bị sửa đổi.
+
+console.log(sentence.replace(/mèo/giu, "cún"));
