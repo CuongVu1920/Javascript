05056ee45b65ef8edea60839d6a6538dcd443a91
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-03 10:13:59 +0700

    Xử lý chuỗi có chứa Emoji

---
+++ String/Xử lý chuỗi có chứa Emoji/main.js
@@ -0,0 +1,15 @@
+// Window + .
+
+console.log("ABC".length); // 3
+
+console.log("😊😒".length); // 4
+
+// length đếm đơn vị nhỏ nhất tạo lên chuỗi => đơn vị đó là code units
+
+// code units: 😒 - 2
+// code units: 😊 - 2
+// code units: ☹️ - 2
+
+console.log(Array.from("😊😒").length); // 2
+
+
