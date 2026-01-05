6e288251a740fb0159d04f7560d905222ae89c5a
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-05 09:59:04 +0700

    kiến thức đã học về số

---
+++ String/Tìm hiểu bảng mã ký tự ASCII/main.js
@@ -0,0 +1,9 @@
+// ASCII:  Là bảng mã hóa ký tự viết tắt của American Standard Code for Information Interchange
+// 7 bit dể mã hóa kỹ tụ: 128 characters
+
+// VNI
+
+// Quôc gia X: Bảng mã X
+// ...
+
+// [0 || 1][][][][][][][]
---
+++ Số học và Math/kiến thức đã học về số/main.js
@@ -0,0 +1,22 @@
+// NaN, isNaN
+// parseInt()
+// Number.prototype.toFixed()
+
+console.log(500000000); // = 5e8 = 5 * 10 ** 8
+
+// NaN - Not a number
+console.log(typeof NaN); // number
+console.log(NaN + 2); // NaN
+console.log(+"abc"); // NaN
+
+// console.log(typeof x === 'number' && !isNaN(x));
+
+
+// Tham số thứ 2 của hàm parseInt(string, radix) radix: đại diện cho hệ số (nhị phân, thập phân, thập lục phân)
+
+console.log(parseInt(100.5)); // 100
+
+
+
+
+
