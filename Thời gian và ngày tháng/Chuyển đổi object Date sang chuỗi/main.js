274f3dba9424918ef96db8d6212f3fd3af213c6e
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-12 16:15:15 +0700

    chuyen doi object sang chuoi

---
+++ Thời gian và ngày tháng/Chuyển đổi object Date sang chuỗi/main.js
@@ -0,0 +1,10 @@
+// console.log(new Date().toISOString()); // 2026-01-12T09:07:02.033Z
+
+console.log(new Date().toLocaleDateString("VN-vi")); // 12/1/2026
+
+
+console.log(new Date().toLocaleDateString("vi"), {
+
+});
+
+console.log(Date);
