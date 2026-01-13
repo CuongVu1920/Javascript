5c20a2c15de982c3c5f6b17e4a5720b869124b43
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-13 10:03:20 +0700

    getElementByID

--- DOM/Tìm hiểu sơ bộ về DOM/main.js
+++ DOM/Tìm hiểu sơ bộ về DOM/main.js
@@ -3,8 +3,25 @@
 // 1. Khái niệm Node trong HTMLNode trong HTML là đơn vị cơ bản trong DOM (Document Object Model), 
 // đại diện cho mọi thành phần của tài liệu HTML. Mọi thứ trong HTML đều là node, bao gồm elements, text, comments, và nhiều loại khác.
 
-console.log(document);
+// console.log(document);
 
 // Element node
 // Attribute node
 // Text node
+
+
+// Lưu ý khi lấy phần tử trong DOM 
+
+// document.getElementById('elementId');
+
+const h1 = document.getElementById('heading');
+
+h1.innerText = "Hello Cuong Vu";
+
+
+/**
+ * Note: 
+ *  - khi có 2 cái id trùng nhau thì nó sẽ trả về phần tử đầu tiên
+ *  - document.getElementById('elementId'); trả về đối tượng chứa id của phần tử đó, không tìm được sẽ trả về null
+ *  - chính cái nội dung của id sẽ tạo ra đối tượng mà chứa id đó
+ */
