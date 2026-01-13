f27151583b7bea756854cd37d553dd9f7d056d72
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-13 09:38:18 +0700

    Document và phân loại các node

---
+++ DOM/Tìm hiểu sơ bộ về DOM/main.js
@@ -0,0 +1,10 @@
+// HTML DOM (Document Object Model) - mô hình thể các phần tử HTML dưới dạng đối tượng
+
+// 1. Khái niệm Node trong HTMLNode trong HTML là đơn vị cơ bản trong DOM (Document Object Model), 
+// đại diện cho mọi thành phần của tài liệu HTML. Mọi thứ trong HTML đều là node, bao gồm elements, text, comments, và nhiều loại khác.
+
+console.log(document);
+
+// Element node
+// Attribute node
+// Text node
