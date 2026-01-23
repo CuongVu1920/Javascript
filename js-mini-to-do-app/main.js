b3e96e8806eaae51e8868e59a56e7d59f19097d7
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-23 11:07:33 +0700

    Kiểm tra tiêu đề bị null và trùng tiêu đề công việc

--- js-mini-to-do-app/main.js
+++ js-mini-to-do-app/main.js
@@ -3,6 +3,7 @@ const tasks = [];
 const taskList = document.getElementById('task-list');
 const formTask = document.querySelector('.todo-form');
 const inputTask = document.querySelector('#todo-input');
+const textNoTasks = document.querySelector('.no-tasks');
 
 function handleTaskAction(event) {
   const taskItem = event.target.closest('.task-item');
@@ -10,8 +11,15 @@ function handleTaskAction(event) {
   const task = tasks[taskIndex];
 
   if(event.target.closest(".task-btn.edit")) {
-    const newTitle = prompt("Nhập tên công việc mới", task.title);
-    if(!newTitle) return alert('Tên công việc không được để trống');
+    let newTitle = prompt("Nhập tên công việc mới", task.title);
+    
+    if(newTitle === null) return; // Người dùng nhấn hủy
+
+    newTitle = newTitle.trim();
+    if(newTitle === '') return alert('Tên công việc không được để trống');
+
+    if(newTitle === task.title) return alert('Tên công việc bị trùng lặp');
+
     task.title = newTitle;
     renderTasks();
    
@@ -27,7 +35,7 @@ function handleTaskAction(event) {
   }else if(event.target.closest(".task-btn.delete")) {
     const isConfirm = confirm(`Bạn có chắc chắn muốn xóa công việc ${task.title} không?`);
     if(isConfirm) {
-      delete tasks[taskIndex];
+      tasks.splice(taskIndex, 1); // array.splice(start, deleteCount, item1, item2, ...)
       renderTasks();
     }
   }
@@ -38,6 +46,10 @@ function addTask(event) {
   event.preventDefault();
     const taskTitle = inputTask.value.trim();
     if(!taskTitle) return alert('Vui lòng nhập tên công việc');
+
+    if(tasks.some(task => task.title === taskTitle)) {
+      return alert('Tên công việc bị trùng lặp');
+    }
 
     tasks.push({
       title: taskTitle,
@@ -50,6 +62,13 @@ function addTask(event) {
 
 // Sự kiện cập nhật task
 function renderTasks() {
+  if(tasks.length === 0) {
+    textNoTasks.textContent = 'No tasks available. Please add a task!';
+    taskList.innerHTML = '';
+    return;
+  }
+
+
   const html = tasks.map((task, index) => {
     return `
       <li class="task-item ${task.completed ? 'completed' : ''}" task-index="${index}">
@@ -62,6 +81,7 @@ function renderTasks() {
       </li>
     `
   }).join('');
+  textNoTasks.textContent = '';
 
   taskList.innerHTML = html;
 }
