0ab327c4bfd3f16554964c00b9b9f5535527c066
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-24 09:38:05 +0700

    Lưu trữ dữ liệu vào LocalStorage

--- js-mini-to-do-app/main.js
+++ js-mini-to-do-app/main.js
@@ -1,13 +1,27 @@
-const tasks = [];
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
 
 const taskList = document.getElementById('task-list');
 const formTask = document.querySelector('.todo-form');
 const inputTask = document.querySelector('#todo-input');
 const textNoTasks = document.querySelector('.no-tasks');
 
+
+function checkDuplicate(listTask, newTitle, excludeIndex = -1) {
+
+  return listTask.some((task, index) => task.title.toLowerCase() === newTitle.toLowerCase() && excludeIndex !== index);  
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// localStorage.setItem() nghĩa là Lưu tasks vào localStorage
+
+
 function handleTaskAction(event) {
   const taskItem = event.target.closest('.task-item');
-  const taskIndex = taskItem.getAttribute('task-index');
+  const taskIndex = +taskItem.getAttribute('task-index');
   const task = tasks[taskIndex];
 
   if(event.target.closest(".task-btn.edit")) {
@@ -18,25 +32,25 @@ function handleTaskAction(event) {
     newTitle = newTitle.trim();
     if(newTitle === '') return alert('Tên công việc không được để trống');
 
-    if(newTitle === task.title) return alert('Tên công việc bị trùng lặp');
+    const hasDuplicate = checkDuplicate(tasks, newTitle, taskIndex); 
+    if(hasDuplicate) return alert('Tên công việc đã tồn tại');
 
     task.title = newTitle;
     renderTasks();
+    saveTasks();
+    return;
    
   }else if(event.target.closest(".task-btn.done")) {
     task.completed = !task.completed;
-    if(task.completed) {
-      alert('Đã hoàn thành công việc');
-    }else {
-      alert('Xác nhận công việc chưa hoàn thành');
-    };
-
     renderTasks();
+    saveTasks();
+    return;
   }else if(event.target.closest(".task-btn.delete")) {
     const isConfirm = confirm(`Bạn có chắc chắn muốn xóa công việc ${task.title} không?`);
     if(isConfirm) {
       tasks.splice(taskIndex, 1); // array.splice(start, deleteCount, item1, item2, ...)
       renderTasks();
+      saveTasks();
     }
   }
 }
@@ -47,9 +61,8 @@ function addTask(event) {
     const taskTitle = inputTask.value.trim();
     if(!taskTitle) return alert('Vui lòng nhập tên công việc');
 
-    if(tasks.some(task => task.title === taskTitle)) {
-      return alert('Tên công việc bị trùng lặp');
-    }
+    const hasDuplicate = checkDuplicate(tasks, taskTitle);  
+    if(hasDuplicate) return alert('Tên công việc đã tồn tại');
 
     tasks.push({
       title: taskTitle,
@@ -57,6 +70,7 @@ function addTask(event) {
     });
 
     renderTasks();
+    saveTasks();
     inputTask.value = '';
 }
 
