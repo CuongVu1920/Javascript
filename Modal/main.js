39d179c88a421d2aa2bb32660b5f39d4b4457a5a
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-25 08:27:41 +0700

    Xử lý nhiều modal hoạt động

--- Modal/main.js
+++ Modal/main.js
@@ -1,30 +1,58 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const openModal = $("#open-modal");
-const modal = $("#modal");
-const closeModal = $("#modal-close");
+let currentModal = null;
 
+const listOpenModal = $$("[data-modal]");
 
-openModal.addEventListener("click", () => {
-  modal.classList.add("show");
+listOpenModal.forEach((btn) => {
+ btn.onclick = function() {
+  modal = $(this.dataset.modal);
 
-})
-
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("show");
+  if(modal) {
+    modal.classList.add("show");
+    currentModal = modal;
+  }else {
+    console.error(`${this.dataset.modal} not found`)
+  }
+ }
 });
 
-modal.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.classList.remove("show");
-  } 
-});
+const modalCloseList = $$(".modal-close");
 
-document.addEventListener('keydown', (event) => {
-  if(event.key === "Escape") {
-    modal.classList.remove("show");
+modalCloseList.forEach((btn) => {
+  btn.onclick = function() {
+    const modal = this.closest(".modal-backdrop");
+    if(modal) {
+      modal.classList.remove("show");
+      currentModal = null;
+    }else {
+      console.error("modal not found")
+    }
   }
 });
+
+const listModal = $$(".modal-backdrop");
+
+listModal.forEach(modal => {
+  modal.onclick = function(event) {
+
+    if(event.target === this) {
+      this.classList.remove("show");
+      currentModal = null;
+    }    
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && currentModal) {
+      currentModal.classList.remove("show");
+      currentModal = null;
+  }
+})
+
+
+
+
 
 
