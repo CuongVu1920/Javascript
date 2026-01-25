570d3e3b608f10557b432321c14c2745f8c63c87
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-25 08:01:37 +0700

    Đóng mở modal

--- Modal/main.js
+++ Modal/main.js
@@ -1,5 +1,30 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-console.log($("#open-modal"));
-console.log($("#modal"));
+const openModal = $("#open-modal");
+const modal = $("#modal");
+const closeModal = $("#modal-close");
+
+
+openModal.addEventListener("click", () => {
+  modal.classList.add("show");
+
+})
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.remove("show");
+  } 
+});
+
+document.addEventListener('keydown', (event) => {
+  if(event.key === "Escape") {
+    modal.classList.remove("show");
+  }
+});
+
+
