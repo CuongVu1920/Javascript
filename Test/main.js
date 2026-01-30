bf85e2ef59891b9021f871d1eda223636fafc037
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-01-30 09:10:34 +0700

    Không có gì đặc biệt

--- Test/main.js
+++ Test/main.js
@@ -1,81 +1,158 @@
+// myForEach
 
-  Array.prototype.mySome = function(callback, thisValue) {
-    const length = this.length;
-
-    for(let i = 0; i < length; i++ ) {
-      if(i in this) {
-
-        if(callback.call(thisValue, this[i], i, this)) {
-          return true;
-        }
-      }
+Array.prototype.myForEach = function(callback, thisArg) {
+  const length = this.length;
+  for(let i = 0; i < length; i++) {
+    if(i in this) {
+      callback.call(thisArg, this[i], i, this);
     }
-    return false;
+  }
+}
+
+
+
+const arr = [1, 2, 3];
+
+arr.forEach((num) => {
+  console.log(num * 2);
+});
+
+arr.myForEach((num) => {
+  console.log(num * 2);
+}); 
+
+
+// 2. myMap()
+
+Array.prototype.myMap = function(callback, thisArg) {
+  const length = this.length;
+  const result = new Array(length);
+
+  for(let i = 0; i < length; i++) {
+    result[i] = callback.call(thisArg, this[i], i, this);
   }
 
-  Array.prototype.myEvery = function(callback, thisValue) {
-    const length = this.length;
+  return result;
+}
 
-    for(let i = 0; i < length; i++) {
-      if(i in this) {
-        if(!callback.call(thisValue, this[i], i, this)) {
-          return false;
-        }
-      }
+
+const mappedArr = arr.map((num) => {
+  return num * 2;
+});
+
+console.log(mappedArr); (3) [2, 4, 6];
+
+const mappedArr2 = arr.myMap((num) => {
+  return num * 2;
+});
+
+console.log(mappedArr2); (3) [2, 4, 6];
+
+
+// myFilter() - CHUẨN
+
+Array.prototype.myFilter = function(callback, thisArg) {
+  const length = this.length;
+  const result = [];
+  
+  for(let i = 0; i < length; i++) {
+    let element = this[i];
+    if(callback.call(thisArg, this[i], i, this)) {
+      result.push(element);
     }
-    return true;
+  }
+  return result;
+}
+
+
+
+const arr3 = [1, 2, 3, 4, 5];
+
+const filteredArr = arr3.filter((num) => {
+  return num % 2 === 0;
+});
+
+console.log(filteredArr); // (2) [2, 4]
+
+
+const filter2 = arr3.filter((num) => {
+  return num % 2 === 0;
+})
+
+console.log(filter2);
+
+
+// Find
+
+Array.prototype.myFind = function(callback, thisArg) {
+  const length = this.length;
+
+  for(let i = 0; i < length; i++) {
+    if(callback.call(thisArg, this[i], i, this)) {
+      return this[i];
+    }
   }
 
-  // ========== TEST CASES ==========
+  return "undefined";
+}
 
-console.log('========== TEST EVERY ==========');
 
-const numbers = [2, 4, 6, 8, 10];
+const obj = { minAge: 18 };
 
-// Test 1: Tất cả đều chẵn
-console.log('Tất cả số chẵn?', numbers.myEvery(num => num % 2 === 0)); // true
+const users = [
+    { name: 'An', age: 15 },
+    { name: 'Bình', age: 20 },
+    { name: 'Chi', age: 25 }
+];
 
-// Test 2: Tất cả > 5
-console.log('Tất cả > 5?', numbers.myEvery(num => num > 5)); // false
+const adult = users.find(function(user) {
+    return user.age >= this.minAge;
+}, obj);
 
-// Test 3: Mảng rỗng (luôn return true)
-console.log('Mảng rỗng:', [].myEvery(num => num > 100)); // true
+console.log(adult);
 
-// Test 4: Với thisArg
-const checker = {
-  threshold: 5,
-  check: function(num) {
-    return num > this.threshold;
+const adult2 = users.myFind(function(user) {
+    return user.age >= this.minAge;
+}, obj);
+
+console.log(adult2); // { name: 'Bình', age: 20 }
+
+// mySome và myEvery
+
+Array.prototype.mySome = function(callback, thisArg) {
+  const length = this.length;
+
+  for(let i = 0; i < length; i++) {
+    if(callback.call(thisArg, this[i], i, this)) {
+      return true;
+    }
   }
-};
-console.log('Với thisArg:', numbers.myEvery(checker.check, checker)); // false
+  return false;
+}
 
 
-console.log('\n========== TEST SOME ==========');
+Array.prototype.myEvery = function(callback, thisArg) {
+  const length = this.length;
+  
+  for(let i = 0; i < length; i++) {
+    if(!callback.call(thisArg, this[i], i, this)) {
+      return false;
+    }
+  }
+  return true;
+}
 
-const scores = [45, 60, 75, 30, 85];
+// findIndex
 
-// Test 1: Có số nào >= 80?
-console.log('Có điểm >= 80?', scores.mySome(score => score >= 80)); // true
-
-// Test 2: Có số nào > 100?
-console.log('Có điểm > 100?', scores.mySome(score => score > 100)); // false
-
-// Test 3: Mảng rỗng (luôn return false)
-console.log('Mảng rỗng:', [].mySome(num => num > 0)); // false
-
-// Test 4: Tìm phần tử cụ thể
-const fruits = ['táo', 'chuối', 'cam'];
-console.log('Có "cam"?', fruits.mySome(fruit => fruit === 'cam')); // true
-console.log('Có "xoài"?', fruits.mySome(fruit => fruit === 'xoài')); // false
+Array.prototype.myFindIndex = function(callback, thisArg) {
+  const length = this.length;
+  
+  for(let i = 0; i < length; i++) {
+    if(callback.call(thisArg, this[i], i, this)) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 
-console.log('\n========== SO SÁNH VỚI BUILT-IN ==========');
-
-const testArray = [1, 2, 3, 4, 5];
-
-console.log('every - built-in:', testArray.every(n => n > 0)); // true
-console.log('every - custom:', testArray.myEvery(n => n > 0)); // true
-
-console.log('some - built-in:', testArray.some(n => n > 4)); // true
-console.log('some - custom:', testArray.mySome(n => n > 4)); // true
