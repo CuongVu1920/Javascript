29a5d7f03382f30435f73f613a28a9e262e6fd56
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-03-20 13:23:31 +0700

    tim hieu ve callstack

---
+++ BatDongBo/CallStack/main.js
@@ -0,0 +1,8 @@
+/**
+ * Callstack trong JS là 1 cấu trúc dữ liệu ngăn xếp (stack) dùng để quản lý thứ tự
+ * thực thi các hàm. Khi một hàm được gọi, nó được đẩy vào call stack;
+ * - khi hàm hoàn thành, nó được lấy ra khỏi stack.
+ * - quá trình này giúp JS biết hàm nào đang chạy, hàm nào sẽ chạy tiếp theo và kiểm tra luồng thực thi mã.
+ * 
+ * => Call stack giúp JavaScript thực hiện cơ chế đồng bộ (synchronous) và xác định thứ tự thực thi các hàm trong chương trình.
+ */
